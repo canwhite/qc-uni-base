779215bb82b98e649b5ad14f8ff0da60bf919872
qiaochao <752716886@qq.com>
2021-04-07 22:18:41 +0800

    base project

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # qc-uni-base
-uni-app，配置了数据请求、ColorUI，h5打包测试成功，base project
+uni-app 数据请求、colorui，base project
+
+## intro
+
+### 1. libs目录里边放了request请求工具    
+>使用方法：
+```
+let res = await this.$request.get({
+        url:"/weather",
+        data:{
+            city:"北京"
+        },
+})
+```
+>post请求只需要置换get关键词为post即可 
+
+### 2. des目录里边放了colorui的使用方法介绍
+>colorui类似于bootstrap  
+>使用的时候主要靠class选择器  
+>以loading为例:  
+```
+<view class="cu-load load-modal">
+    <image src="/static/logo.png" mode="aspectFit"></image>
+    <view class="gray-text">加载中...</view>
+</view>
+
+```
+>class="cu-load load-modal"  
+>class="gray-text"  
+>des目录里有类似上边的使用方法
+
+
+## run
+> 直接用的HBuilderX,官方ide    
+> 菜单栏=>运行=>运行到浏览器或者其它设备
+
+## PS:
+[自定义tabbar](https://github.com/canwhite/qc-uni-tabbar-custom)
---
+++ pages/index/index.vue
@@ -0,0 +1,69 @@
+<template>
+	<view class="content">
+		<cu-custom class="my_bg" :isBack="true">
+			<!--<block slot="backText">返回</block> -->
+			<block slot="content">base</block>
+			<block slot="right">
+				<view class="action">
+					<!-- <view class="cu-load load-cuIcon" :class="!isLoad?'loading':'over'"></view> -->
+				</view> 
+			</block> 
+		</cu-custom> 
+	</view>
+</template>
+
+<script>
+	// import {mapState} from 'vuex';
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				page: 'index',
+				showPage: false,
+				containerHeight: 0,
+			}
+		},
+		computed: {
+			// ...mapState({
+			// 	themeColor: 'themeColor',
+			// 	tabbarTextColor: "tabbarTextColor",
+			// 	tabbarTextActiveColor: "tabbarTextActiveColor",
+			// })
+		},
+		
+		methods:{
+
+					
+		},
+		onLoad() {
+			
+		},
+	
+		onReady() {
+			
+			
+			//跨域问题，简单解决办法就是用HBuilder内置浏览器运行
+			(async ()=>{
+				
+				let res = await this.$request.get({
+						url:"/weather",
+	                    data:{
+	                        city:"北京"
+	                    },
+				})
+				console.log('------',res);
+				
+			})()
+		},
+
+
+	}
+</script>
+
+<style lang="scss">
+	.my_bg{
+		//渐进色的变化
+		background-image: linear-gradient(45deg, #ff9700, #ed1c24);
+		color: #ffffff;
+	}
+</style>
